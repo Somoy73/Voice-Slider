d7f159302b883f733002f042e67507936471c1fe
Somoy73 <Somoy97@gmail.com>
2020-07-13 21:52:39 +0600

    added slider basic

---
+++ docs/slider.js
@@ -0,0 +1,21 @@
+var imgUrl = ["./1.jpg","./2.jpg","./3,gif"];
+var index = 0;
+
+function implementLeftButton(){
+    var b1 = document.getElementById('nb1');
+    b1.addEventListener('click',function(){
+        let x= document.getElementById('imgsld1');
+        index = (--index<0)? imgUrl.length-1:index;
+        x.src = imgUrl[index]; 
+    });
+}
+function implementRightButton(){
+    var b2 = document.getElementById('nb2');
+    b2.addEventListener('click',function(){
+        let x = document.getElementById('imgsld1');
+        index = (index+1)%imgUrl.length;
+        x.src = imgUrl[index];
+    });
+}
+implementLeftButton();
+impelementRightButton();
